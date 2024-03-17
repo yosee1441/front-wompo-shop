ceed90061ee7954e38367d8e0ea7c7c2b1735363
duvanflor <duvan.flor@netto.com.co>
2024-03-17 12:32:54 -0500

    Fix: Create slideshow for images product

---
+++ src/components/Slideshow/ProductSlideshow.component.tsx
@@ -0,0 +1,48 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
+import { Box } from '@mui/material'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import './productSlideshow.css'
+
+interface ProductSlideShowProps {
+  images: string[]
+  title: string
+}
+
+function ProductSlideshow({ images, title }: ProductSlideShowProps) {
+  return (
+    <Box>
+      <Swiper
+        style={
+          {
+            '--swiper-navigation-color': '#fff',
+            '--swiper-pagination-color': '#fff',
+          } as React.CSSProperties
+        }
+        spaceBetween={10}
+        navigation={true}
+        autoplay={{
+          delay: 2500
+        }}
+        modules={[FreeMode, Navigation, Thumbs]}
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image}>
+            <img
+              src={`/products/${image}`}
+              width={1024}
+              height={800}
+              alt={title}
+              loading="lazy"
+              className='product-slideshow__image'
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  )
+}
+
+export default ProductSlideshow
---
+++ src/components/Slideshow/index.ts
@@ -0,0 +1,1 @@
+export { default as ProductSlideshow } from './ProductSlideshow.component'
