6c069436a648b8f2fe0989b360fc5a8a8e8f3a9d
duvanflor <duvan.flor@netto.com.co>
2024-03-17 12:37:22 -0500

    Fix: Creation pages home, not found and product

--- src/pages/Product/index.ts
+++ src/pages/Product/index.ts
@@ -1,1 +1,1 @@
-export { default as Product } from './product.page'
+export { default as Product } from './Product.page'
--- src/routers/app.router.tsx
+++ src/routers/app.router.tsx
@@ -4,18 +4,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CircularProgress from '@mui/material/CircularProgress'
 import { Navbar } from '@/components/Navbar'
 
-const HomePage = lazy(() => import('@/pages/Home/home.page'))
-const ProductPage = lazy(() => import('@/pages/Product/product.page'))
-const NotFoundPage = lazy(() => import('@/pages/NotFound/notFound.page'))
+const HomePage = lazy(() => import('@/pages/Home/Home.page'))
+const ProductPage = lazy(() => import('@/pages/Product/Product.page'))
+const PurchasePage = lazy(() => import('@/pages/Purchase/Purchase.page'))
+const NotFoundPage = lazy(() => import('@/pages/NotFound/NotFound.page'))
 
 function App() {
   return (
     <Suspense fallback={<CircularProgress />}>
-      <Navbar />
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/product/:slug" element={<ProductPage />} />
+          <Route path="/producto/:slug" element={<ProductPage />} />
+          <Route path="/compra/:id" element={<PurchasePage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
