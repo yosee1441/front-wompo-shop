92b3a527d4a545ce69818d4a986ce1aaa3704861
duvanflor <duvan.flor@netto.com.co>
2024-03-17 12:32:10 -0500

    Fix: Create component Navbar

--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -1,17 +1,15 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
+import { Link } from 'react-router-dom'
 import Typography from '@mui/material/Typography'
-import MenuIcon from '@mui/icons-material/Menu'
 
 function Navbar() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" color="transparent" elevation={1}>
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6">TESLO SHOP</Typography>
+        <Link to={'/'}>
+          <Typography variant="h6">WOMPO SHOP</Typography>
+        </Link>
       </Toolbar>
     </AppBar>
   )
